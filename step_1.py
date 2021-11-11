6490e1131e1e53f6c4c15c36584a3eb7fd6aafc1
Mike <michael.pursell@gmail.com>
2021-11-11 14:28:30 +0000

    added mod operator

---
+++ step_1.py
@@ -0,0 +1,30 @@
+import sys
+
+def calculate(operator, value_1, value_2) -> int:
+    param_1 = int(value_1)
+    param_2 = int(value_2)
+
+    if operator == '+':
+        return param_1 + param_2
+    elif operator == '-':
+        return param_1 - param_2
+    elif operator == 'x':
+        return param_1 * param_2
+    elif operator == '/':
+        return param_1 / param_2
+    elif operator == '%':
+        # do some stuff
+        return param_1 % param_2
+    else:
+        raise Exception("Unexpected Operator!")
+
+def main():
+    if len(sys.argv) < 4:
+        print("Missing Arguments To Calculator")
+        return
+
+    print(calculate(sys.argv[1], sys.argv[2], sys.argv[3]))
+    return 
+
+if __name__ == "__main__":
+    main()
